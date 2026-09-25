d788714f43bfcea5efd561f5488f9d8ab1fa9b14
Mohammed Megahed <mohammedmegahed2010@gmail.com>
2026-09-25 21:08:02 +0300

    Accepted: 2266A Good Contest (15 ms, 0 KB)

---
+++ 2266A-Good-Contest/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while (t--){
+        short int n, weak=SHRT_MAX; cin >> n;
+        for (short int i = 0; i < 3; i++){
+            short int s; 
+            cin >> s;
+            if (s < weak) weak = s;
+        } cout << n-weak << '\n';
+    }
+}
